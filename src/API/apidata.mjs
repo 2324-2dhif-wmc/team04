133230dbc597d73dea023789b96aed8edbdba693
Manuel Groß <m.gross@HTBLALeonding.onmicrosoft.com>
2024-04-18 10:11:58 +0200

    formateed api data

--- src/API/apidata.mjs
+++ src/API/apidata.mjs
@@ -1,8 +1,29 @@
 import { restClient } from '@polygon.io/client-js';
 const rest = restClient('hfWWLRN1okw1sX49EmIQ1ccVwaS1GbdF');
 
-rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14").then((data) => {
-    console.log(data);
-}).catch(e => {
-    console.error('An error happened:', e);
-});
+
+export function getThreeMonthRange(symbol, from, to) {
+    const cValues = [];
+    rest.stocks.aggregates(symbol, 1, "day", from, to).then((data) => {
+        for(let i = 0; i < data.results.length; i++) {
+            cValues.push(data.results[i].c);
+        }
+    });
+    return [cValues, from, to]
+}
+
+export function generateDateRangeArray(startDate, endDate) {
+    const dateArray = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= new Date(endDate)) {
+        dateArray.push(currentDate.toISOString().slice(0, 10));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dateArray;
+}
+
+
+const startDate = "2023-01-01";
+const endDate = "2023-03-01";
--- src/API/chart.mjs
+++ src/API/chart.mjs
@@ -1,3 +1,4 @@
+
 var options = {
     chart: {
         type: 'line'
