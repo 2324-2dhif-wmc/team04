127fcc5348b78f0cb5fd00213118f6a2423226ae
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2024-05-17 08:22:26 +0200

    add getUser

--- src/ServerClient/serverClient.mjs
+++ src/ServerClient/serverClient.mjs
@@ -1,3 +1,5 @@
+import {Stock, User} from "../model.mjs";
+
 const serverUrl = 'http://localhost:3000/users';
 
 export function updateUser(user) {
@@ -10,4 +12,33 @@ export function updateUser(user) {
     })
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
+}
+
+export function getUser(email, callback)
+{
+    fetch(`${serverUrl}?email=${email}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(users => {
+            const user = users[0];
+            if (!user) {
+                console.log('Benutzer existiert nicht');
+                return;
+            }
+
+            let us =  new User(user.id, user.email, user.password);
+
+            callback(user, null);
+
+        })
+        .catch(error => {callback(null, error);});
 }
--- src/model.mjs
+++ src/model.mjs
@@ -11,7 +11,6 @@ export class Stock
     }
 }
 
-
 export class News
 {
     constructor(symbol, time, headline, img, src, summary, url)
@@ -28,10 +27,11 @@ export class News
 
 export class User
 {
-    constructor(id, email, password) {
+    constructor(id, email, password, name) {
         this.id = id;
         this.email = email;
         this.password = password;
+        this.name = name;
         this.money = 0;
         this.stocks = [];
     }
