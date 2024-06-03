382a3bd235413c64afe49339e2f299cca2da73a7
Michael Oswald <m.oswald@HTBLALeonding.onmicrosoft.com>
2024-06-03 20:00:41 +0200

    bissi wos pfuscht. schlussendlich nur wos am footer geändert

--- content/assets/js/teamCode/main.mjs
+++ content/assets/js/teamCode/main.mjs
@@ -23,3 +23,24 @@ let x = document.getElementById("Account");
 x.innerHTML = fixedUser.name;
 
 localStorage.setItem('currentUser', JSON.stringify(fixedUser));
+
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    let x1 = document.getElementById("link1");
+    let x2 = document.getElementById("link2");
+    let x3 = document.getElementById("link3");
+    let x4 = document.getElementById("link4");
+    x1.addEventListener("click", function(event) {
+        window.location.href = "#marketStats";
+    })
+    x2.addEventListener("click", function(event) {
+        window.location.href = "#";
+    })
+    x3.addEventListener("click", function(event) {
+        window.location.href = "#";
+    })
+    x4.addEventListener("click", function(event) {
+        window.location.href = "#";
+    })
+})
