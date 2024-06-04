df5f7c88a82fc4799ec7495215a25c72c815620c
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2024-06-04 10:16:16 +0200

    add getMarketStatus and getHoliday

--- content/assets/js/teamCode/API/apidata.mjs
+++ content/assets/js/teamCode/API/apidata.mjs
@@ -1,27 +1,5 @@
 import {getDateString, Stock} from "../model.mjs";
 
-
-export async function getStockNews(symbol)
-{
-    let beginning = new Date();
-    beginning.setDate(beginning.getDate() - 1);
-
-    const apiKey = "cnk1a91r01qvd1hlrv30cnk1a91r01qvd1hlrv3g";
-
-    const url = `https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=${getDateString(beginning).toString()}&to=${getDateString(new Date())}&token=${apiKey}`
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP-Fehler! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-        return null;
-    }
-}
-
 export async function getRange(symbol) {
     try {
         let d = new Date();
@@ -86,4 +64,55 @@ export async function getQuote(symbol) {
     }
     const data = await response.json();
     return new Stock(symbol, data.c, data.h, data.l, data.o, data.t);
+}
+
+export async function getMarketStatus() {
+    try {
+        const apiKey = "cnk1a91r01qvd1hlrv30cnk1a91r01qvd1hlrv3g";
+
+        let url = `https://finnhub.io/api/v1/stock/market-status&exchange=US?token=${apiKey}`;
+
+        let resp = await fetch(url);
+        let data = await resp.json();
+
+        return [data.isOpen, data.session];
+
+    } catch (error) {
+        console.log("Fehler beim Abrufen der Daten:", error);
+    }
+}
+
+export async function getHoliday() {
+    try {
+        const apiKey = "cnk1a91r01qvd1hlrv30cnk1a91r01qvd1hlrv3g";
+
+        let url = `https://finnhub.io/api/v1/stock/market-holiday&exchange=US?token=${apiKey}`;
+
+        let resp = await fetch(url);
+        return await resp.json();
+    } catch (error) {
+        console.log("Fehler beim Abrufen der Daten:", error);
+    }
+
+}
+
+export async function getStockNews(symbol)
+{
+    let beginning = new Date();
+    beginning.setDate(beginning.getDate() - 1);
+
+    const apiKey = "cnk1a91r01qvd1hlrv30cnk1a91r01qvd1hlrv3g";
+
+    const url = `https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=${getDateString(beginning).toString()}&to=${getDateString(new Date())}&token=${apiKey}`
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP-Fehler! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+        return null;
+    }
 }
