e97af999a2fe5e6368e013ee8915684a1ef51f04
SchwendtbauerJulian <j.schwendtbauer@students.htl-leonding.ac.at>
2024-06-06 07:48:40 +0200

    update apidata.mjs

--- content/assets/js/teamCode/API/apidata.mjs
+++ content/assets/js/teamCode/API/apidata.mjs
@@ -38,8 +38,8 @@ export async function getTodayStock(symbol) {
         from = getDateString(from);
         let to = getDateString(new Date());
 
-        //let url = `https://api.polygon.io/v2/aggs/ticker/AAPL/range/30/minute/${from}/${to}?adjusted=true&sort=asc&apiKey=${jpolygonKey}`;
-        let url = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/30/minute/2024-06-05/2024-06-06?adjusted=true&sort=asc&apiKey=aEMjzbpWJ5Z0qeGSofwG4_LDJoM9LN_5";
+        let url = `https://api.polygon.io/v2/aggs/ticker/${symbol}/range/30/minute/${from}/${to}?adjusted=true&sort=asc&apiKey=${jpolygonKey}`;
+        //let url = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/30/minute/2024-06-05/2024-06-06?adjusted=true&sort=asc&apiKey=aEMjzbpWJ5Z0qeGSofwG4_LDJoM9LN_5";
 
         let resp = await fetch(url);
         let data = await resp.json();
