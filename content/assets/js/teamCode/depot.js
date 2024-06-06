aeb5358cd609bde1bc8d937bc1d0dea36a89153b
Michael Oswald <m.oswald@HTBLALeonding.onmicrosoft.com>
2024-06-06 09:27:16 +0200

    depot fertig, bis auf das sell

--- content/assets/js/teamCode/depot.js
+++ content/assets/js/teamCode/depot.js
@@ -1,17 +1,21 @@
-import {getUser, updateUser, sellStock} from "./ServerClient/serverClient.mjs";
-import {getQuote} from "./API/apidata.mjs";
-import {User} from "./model.mjs";
+import { sellStock } from "./ServerClient/serverClient.mjs";
+import { getQuote } from "./API/apidata.mjs";
+import { User } from "./model.mjs";
 
-const table = document.getElementsByTagName("depotTable");
+const table = document.getElementById("depotTable");
 let d = JSON.parse(localStorage.getItem('currentUser'));
 let user = new User(d.id, d.name, d.email, d.password, d.money, d.stocks);
 
+function umleiten(symbol) {
+    const basisUrl = "../content/stockInfo.html?symbol=";
+    window.location.href = `${basisUrl}` + symbol;
+}
+
+window.umleiten = umleiten;
+
 async function buildTable() {
     let money = document.getElementById("wert");
-    let info;
     money.innerText = "Money: " + user.money.toFixed(2) + " USD";
-    let table = document.getElementById("depotTable");
-    let n = user.stocks.length;
 
     for (const stock of user.stocks) {
         let symbol = stock.symbol;
@@ -20,30 +24,26 @@ async function buildTable() {
         let quote = await getQuote(symbol);
         let actualValue = quote.currentPrice;
 
-        let imagePath="../content/assets/images/icon/market-value/trends-down-icon.png";
+        let imagePath = "../content/assets/images/icon/market-value/trends-down-icon.png";
         if (val <= actualValue) {
-            imagePath = "../content/assets/images/icon/market-value/trends-up-icon.png"
+            imagePath = "../content/assets/images/icon/market-value/trends-up-icon.png";
         }
         let winLose = (actualValue - val) * stock.amount;
 
         let newRow = table.insertRow(-1);
-        newRow.onclick = function () {
-            window.location.href = "../content/stockInfo.html";
-        }
-        let img = document.createElement("img");
-        img.src = imagePath;
-        img.alt = "Dynamic Image";
+
         newRow.innerHTML = `
-            <td>${symbol}</td>
+            <td onclick="umleiten('${symbol}')">${symbol}</td> 
             <td>${stock.amount}</td>
             <td>${val.toFixed(2)}</td>
             <td>${actualValue.toFixed(2)}</td>
-            <td style="padding-left: 3%"><img src=${imagePath} alt="Impact"></td>
+            <td style="padding-left: 3%"><img src="${imagePath}" alt="Impact"></td>
             <td>${winLose.toFixed(2)}</td>
-            <td><button class="btn btn-primary" onclick="sellStock('${stock}')">Click Me</button></td>
-            `;
+            <td>
+                <button class="btn btn-primary" onclick="sellStock('${JSON.stringify(stock)}')">Sell</button>
+            </td>
+        `;
     }
 }
 
 buildTable();
-
